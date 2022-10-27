2e050f2b418ab5c7967418aca233a8d6aec07718
Jonathan Ostertag <jo.ostertag@tum.de>
2022-10-27 14:33:23 +0200

    feat: add time measurement

--- complicated-circuit.py
+++ complicated-circuit.py
@@ -1,3 +1,4 @@
+import time
 from simulation import *
 
 """
@@ -7,13 +8,15 @@ N = Number of qubits; D = Number of 2-qubit gates on each qubit
 """
 
 N = 8
-D = 20
+D = 200
 
 
 # setting up the circuit with first state
+print("Setting up circuit...")
 bit_sequence = N * [0]
 circuit = Circuit(bit_sequence)
 
+print("Adding Gates...")
 for d in range(D):
     # adding one qubit gates first
     for n in range(N):
@@ -25,8 +28,13 @@ for d in range(D):
         circuit.add_gate(GATES.cx, n, n+1)
         n = n + 2
 
+print("Start running the circuit...")
+startTime = time.time()
 circuit.run()
+endTime = time.time()
+duration = '{:5.3f}s'.format(endTime-startTime)
+print(f'Done in {duration}!')
 
 sv = circuit.state_vector()
-print(sv)
+# print(sv)
 
